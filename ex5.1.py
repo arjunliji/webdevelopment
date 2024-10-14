2bb43e694bc78092fda64b6c6733a91b83e22fff
Arjun <76899772+arjunliji@users.noreply.github.com>
2024-10-14 21:53:17 +0530

    idk what this is. I just wanted to make random pr xoxo

--- ex5.1.py
+++ ex5.1.py
@@ -1,8 +1,8 @@
 num = 0
 total = 0.0
-while True :
+while True:
     sval = input('Enter a number: ')
-    if sval == 'done' :
+    if sval == 'done' or sval = '':
         break
     try:
         fval = float(sval)
@@ -10,9 +10,9 @@ while True :
         print('invalid input')
         continue
     print(fval)
-    num=num+1
-    total=total+fval
+    num += 1
+    total += fval
 
 print('all done')
-print(total,num,(total/num))
+print(total, num, (total/num))
 
